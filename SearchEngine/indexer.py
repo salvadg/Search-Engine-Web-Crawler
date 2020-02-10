94366d5fe34fa77eb927b347a45e04e98197d967
Salvador <salvadjg@uci.edu>
2020-02-09 21:32:35 -0800

    Add files via upload

---
+++ SearchEngine/indexer.py
@@ -0,0 +1,156 @@
+
+## Creates index for future queries
+## Salvador Gutierrez (89291450)
+## 03/08/2019 @ 3:52 p.m
+
+
+import nltk
+import json
+import pickle
+from math import log
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+# nltk.download('punkt')
+from bs4 import BeautifulSoup, Comment
+from collections import defaultdict
+import re
+import os
+from sys import getsizeof
+
+dict_index = defaultdict(dict)
+
+_JPATH = "WEBPAGES_RAW/bookkeeping.json"
+# _JPATH2 = "TEST/bookkeeping.json"
+_DOCS = 0
+stop_words = set(stopwords.words('english'))
+_UNIQUE_WORDS = set()
+
+
+
+def query(db,input):
+
+    result = []
+    ranked = dict()
+    global _JPATH
+
+    with open(_JPATH) as file:
+        obj = json.load(file)
+
+        for token in input:
+            docs = db[token.lower()]
+            for doc_id, rank in docs.items():  ##{token: {doc_id,tf-idf}}
+                if doc_id in ranked:
+                    ranked[doc_id] = rank *1.5 ## if the doc_id appears again increase the rank
+                else: ## else not in ranked dict already so just add it
+                    ranked[doc_id] = rank
+
+
+        ## after we populate new ranked dictionary get resulting urls sorted by rank
+        for doc_id, rank in sorted(ranked.items(), key = lambda x: x[1], reverse =True): 
+            # print doc_id, rank
+            result.append(obj[doc_id]) ## add url
+
+    return result
+
+
+
+
+## parse document passed in
+def parse_doc(document):
+    with open(document) as f:
+        try:
+            soup = BeautifulSoup(f.read(), 'lxml') ## parse page
+            clean_soup(soup) ## clean html
+        except ParseError:
+            print("COULD NOT PARSE!!")
+    return soup        
+
+        
+
+def tokenize_data(data):
+    
+    ## format-> {token: [(doc_id,tf-idf)]}
+    t = re.findall(r'[a-zA-Z]{3,}',data) ## excludes numbers
+    return t
+
+    
+
+def initialize_index():
+    global _DOCS
+    _PATH = "./WEBPAGES_RAW"
+
+    _IDF = 0
+    # _PATH = "TEST"
+    index = defaultdict(dict)
+    result = defaultdict()
+    global stop_words
+    for subdir, dirs, files in os.walk(_PATH):
+        
+        for file in files:
+            ## I only want the html documents
+            if not file.endswith('.json') and not file.endswith('.tsv') and '.DS_Store' not in file:
+                doc_path = os.path.join(subdir,file) ## create path to open file
+                _DOCS += 1
+                # id = doc_path.replace("./TEST","") ## get doc_id
+                id = doc_path.replace("./WEBPAGES_RAW/","") ## get doc_id
+
+                html_text = parse_doc(doc_path)
+
+                extract_tag(index,html_text,id,"body")
+                extract_tag(index,html_text,id,"title")
+                extract_tag(index,html_text,id,"strong")
+                extract_tag(index,html_text,id,"p")
+                extract_tag(index,html_text,id,"h1")
+
+    ## update index values to TF_IDF
+    for docs in index.values():
+        _IDF = log(float(_DOCS)/len(docs))
+        for k,tf in docs.items():
+            docs[k] = _IDF * tf
+    return index
+
+def extract_tag(index,html_text, doc_id, tag):
+
+    _frequency = 0 
+    extracted = defaultdict(int)
+    for t in html_text.find_all(tag):
+        data = t.text
+        all_tokens = tokenize_data(data.lower())
+        for token in all_tokens:
+            if token not in stop_words: ## ignore stop_words
+                extracted[token]+=1 ## {token:term_frequency}
+
+        for key, tf in extracted.items():
+            _frequency = 1+log(float(tf))
+
+            index[key].update({doc_id:_frequency}) ## format -> {"token":{doc1:tf}}
+
+def create_pickle(index):
+    with open('index.pickle','wb') as pick:
+        pickle.dump(index, pick, protocol=pickle.HIGHEST_PROTOCOL)
+
+def output():
+    global _DOCS
+    global _UNIQUE_WORDS
+    global dict_index
+    with open("index_db.txt", "w") as file:
+        file.write("\nNumber of Documents: {}\n".format(_DOCS) )
+        file.write("\nTotal Unique Words: {}\n".format(len(_UNIQUE_WORDS)))
+        KB = getsizeof(dict_index)/1000
+        file.write("\nSize of Index {} KB\n".format(KB))
+
+def load_pickle(p):
+    with open(p, 'rb') as pick:
+        data = pickle.load(pick)
+        return data
+
+def clean_soup(soup):
+    [s.extract() for s in soup.find_all('script')] ## remove script from html
+    [s.extract() for s in soup.find_all('style')]
+    [s.extract() for s in soup.find_all('meta')] ## remove meta data
+    [s.extract() for s in soup.find_all(text = lambda text:isinstance(text, Comment))] ## remove html comments
+    
+    return soup ## return cleaned html
+
+
+
---
+++ SearchEngine/searchGUI.py
@@ -0,0 +1,93 @@
+## Search GUI 
+## Salvador Gutierrez (89291450)
+## 03/08/2019 @ 7:12 p.m
+import indexer as dex
+import Tkinter as tk
+from sys import getsizeof
+
+HEIGHT = 500
+WIDTH = 600
+
+SELECTED = -1
+
+def start(db):
+    root = tk.Tk()
+
+    canvas = tk.Canvas(root, bg = 'gray', height = HEIGHT, width =WIDTH)
+    canvas.pack(fill = tk.BOTH)
+
+    ## title of search engine frame
+    titleFrame = tk.Frame(canvas, bg = "#0c2340")
+    titleFrame.place(relx = 0.05,rely = 0.01, relwidth = 0.9, relheight = 0.09)
+    # text = "CS 121 Search Engine"
+    title = tk.Label(titleFrame, bg = "#0c2340", text = "CS 121 Search Engine", font = ("Helvetica", 30), fg = "#F8B229").pack(fill = tk.BOTH)
+    
+
+    outputFrame = tk.Frame(canvas, bg = '#0c2340')
+    outputFrame.place(relx = 0.05,rely = 0.25,relwidth = 0.9, relheight = 0.7)
+
+    ## ## Display Box
+    outputBox = tk.Text(outputFrame,fg = '#0c2340', bg = '#F8B229', font ='Consolas 15', highlightbackground = '#F8B229')
+    outputBox.place(relx = 0.025,rely= 0.025,relwidth = 0.95, relheight = 0.95)
+
+    ## frame container for search box
+    searchFrame = tk.Frame(canvas, bg = '#0c2340')
+    searchFrame.place(relx = 0.05,rely = 0.1, relwidth = 0.9, relheight = 0.15)
+
+
+    ## Search Entry
+    queryField = tk.Entry(searchFrame, highlightbackground ='#0c2340', font ="Consolas 20")
+    queryField.insert(tk.END, 'Enter search')
+    queryField.place(relx = 0.02,rely = 0.10, relwidth = 0.65, relheight = 0.8)
+    queryField.bind("<Button-1>", lambda f:queryField.delete(0, "end"))
+
+    ## Search Button
+    button = tk.Button(searchFrame, text = "Search", highlightbackground = '#0c2340', command = lambda: search(queryField.get(), outputBox))
+    button.place(relx = 0.70, rely= 0.2, relwidth = 0.25, relheight = 0.50)
+
+    ## radio buttons 
+    v = tk.IntVar()
+    topTenRadio = tk.Radiobutton(root,text = "Top 10 Results", command = lambda:radio(v.get()), variable =v, value = 10).pack(anchor = tk.N)
+    topTwentyRadio = tk.Radiobutton(root, command = lambda:radio(v.get()), variable =v, text = "Top 20 results", value = 20).pack(anchor = tk.N)
+    allRadio = tk.Radiobutton(root, text = "Show All results", command = lambda:radio(v.get()), variable =v, value = 0).pack(anchor = tk.N)
+    v.set(0)
+    root.mainloop()
+
+def radio(v):
+    global SELECTED
+    SELECTED = v
+    print(SELECTED)
+
+def search(input, outputBox):
+    outputBox.delete('1.0', tk.END)
+    term = input
+    count = 0
+    with open("links.txt",'w') as file:
+        size = float(getsizeof(db))
+        file.write("\nindex size in KB: {}\n\n".format(size))
+        
+        results = dex.query(db,term.split())
+        outputBox.insert(tk.INSERT, "{} TOTAL RESULTS FOUND FOR \"{}\"\n\n".format(len(results), term ) )
+        for url in results:
+            count+=1
+            outputBox.insert(tk.INSERT, "{}:  {}\n".format(count, url))
+            file.write("{}:  {}\n".format(count, url))
+            if count == SELECTED:
+                count = 0
+                break
+        outputBox.insert(tk.END, "\nSearch Complete!\n")
+
+
+
+
+if __name__ == '__main__':
+
+
+    # index = initialize_index() 
+    # create_pickle(index)
+
+    print("loading database.....")
+    db = dex.load_pickle("index.pickle")
+    start(db)
+
+
